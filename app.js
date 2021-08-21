177f85daf4da78f128420c93d59959e9d138439f
mhrazib121 <mhrazib121@gmail.com>
2021-08-21 10:59:28 +0600

    storage and delivery cost updated

--- app.js
+++ app.js
@@ -16,3 +16,43 @@ function memoryCost(extraPrice){
     memoryCost.innerText=newCost;
     console.log(newCost);
 }
+// Extra Storage cost \\
+
+document.getElementById('256gb-ssd').addEventListener('click', function(){
+    storageCost(0)
+})
+document.getElementById('512gb-ssd').addEventListener('click', function(){
+storageCost(100)
+})
+document.getElementById('1tb-ssd').addEventListener('click', function(){
+    storageCost(180)
+})
+
+function storageCost(extraPrice){
+    const storageCost = document.getElementById('storage-cost');
+    
+    // const memoryCostNumber = parseInt(memoryCost.innerText);
+    const previosCost = 0; 
+    const newCost = previosCost + extraPrice;
+    storageCost.innerText=newCost;
+    console.log(newCost);
+}
+// Delivery Charge \\
+
+document.getElementById('prime-delivery').addEventListener('click', function(){
+    deliveryCharge(0)
+})
+document.getElementById('quick-delivery').addEventListener('click', function(){
+    deliveryCharge(20)
+})
+
+
+function deliveryCharge(extraPrice){
+    const deliveryCost = document.getElementById('delivery-charge');
+    
+    // const memoryCostNumber = parseInt(memoryCost.innerText);
+    const previosCost = 0; 
+    const newCost = previosCost + extraPrice;
+    deliveryCost.innerText=newCost;
+    console.log(newCost);
+}
