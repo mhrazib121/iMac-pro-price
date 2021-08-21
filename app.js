2b8e3fcf7f93f223a723181039bb16efd56c5777
mhrazib121 <mhrazib121@gmail.com>
2021-08-21 10:42:11 +0600

    extra memory cost updated

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log ('iamready')
+// Extra Memory cost \\
+
+document.getElementById('8gb-btn').addEventListener('click', function(){
+memoryCost(0)
+})
+document.getElementById('16gb-btn').addEventListener('click', function(){
+memoryCost(180)
+})
+
+function memoryCost(extraPrice){
+    const memoryCost = document.getElementById('memory-cost');
+    
+    // const memoryCostNumber = parseInt(memoryCost.innerText);
+    const previosCost = 0; 
+    const newCost = previosCost + extraPrice;
+    memoryCost.innerText=newCost;
+    console.log(newCost);
+}
